077938a8890aec6b19df81f53c8bd22c877aed7b
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-07 18:03:07 -0700

    :sparkles:feat: add AtomButton component and variables for button styles

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,28 +1,5 @@
 import { css, variables } from "@stellaria/comet";
-
-const vars = variables({
-  button: {
-    base: {
-      background: "#0072f5",
-      hoverBackground: "#0062d5",
-      border: "#0062d5",
-      text: "#ffffff",
-      boxShadow: "0 0 0.5rem rgba(0, 0, 0, 0.1)",
-    },
-    success: {
-      background: "#00d1b2",
-      hoverBackground: "#00c4a7",
-      border: "#00c4a7",
-      text: "#ffffff",
-    },
-    error: {
-      background: "#e71f33",
-      hoverBackground: "#d61d30",
-      border: "#a51220",
-      text: "#ffffff",
-    },
-  },
-});
+import AtomButton from "../components/atoms/AtomButton";
 
 const wrapper = css`
   width: 100%;
@@ -41,30 +18,11 @@ const content = css`
   align-items: center;
 `;
 
-const button = css`
-  background-color: ${vars?.button?.base?.background};
-  width: max-content;
-  padding: 0.7rem 1rem;
-  border-radius: 0.32rem;
-  border: 2px solid ${vars?.button?.base?.border};
-  color: ${vars?.button?.base?.text};
-  box-shadow: ${vars?.button?.base?.boxShadow};
-  &:hover {
-    background-color: ${vars?.button?.base?.hoverBackground};
-    transform: scale(1.12);
-  }
-  &:active {
-    transform: scale(0.98);
-  }
-
-  transition: all 0.2s ease-in-out;
-`;
-
 const App = () => {
   return (
     <div className={wrapper}>
       <div className={content}>
-        <button className={button}>Server Side Button </button>
+        <AtomButton />
       </div>
     </div>
   );
