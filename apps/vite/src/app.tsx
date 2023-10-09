4f6f39d7c2b0b058ab134d7ffe5d5f31a1acf015
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-09 00:29:25 -0700

    feat(app): add variables for wrapper background color

--- apps/vite/src/app.tsx
+++ apps/vite/src/app.tsx
@@ -1,12 +1,21 @@
 import { css } from "@stellaria/comet";
 import AtomButton from "./components/atom/button";
 
+import { variables } from "@stellaria/comet";
+
+const vars = variables({
+  wrapper: {
+    backgroundColor: "red",
+  },
+});
+
 const wrapper = css`
   width: 100%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: ${vars.wrapper.backgroundColor};
 `;
 
 const content = css`
